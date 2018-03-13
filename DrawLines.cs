7907eb8a3100bae322fd7770c6f1fd0b9655734c
Andrew Mathies <awmathie@indiana.edu>
2018-03-13 12:20:47 -0400

    adding other files that were in different directory

---
+++ DrawLines.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLines : MonoBehaviour {
+
+    public Material lineMat;
+    public Vector3[] points;
+
+    private MapReader map;
+
+    void Start()
+    {
+        map = FindObjectOfType<MapReader>();
+    }
+
+    // Connect all of the `points` to the `mainPoint`
+    void DrawConnectingLines()
+    {
+        foreach (OsmWay w in map.ways)
+        {
+            if (w.Visible)
+            {
+                for (int i = 1; i < w.NodeIDs.Count; i++)
+                {
+                    OsmNode p1 = map.nodes[w.NodeIDs[i - 1]];
+                    OsmNode p2 = map.nodes[w.NodeIDs[i]];
+
+                    Vector3 v1 = p1 - map.bounds.Centre;
+                    Vector3 v2 = p2 - map.bounds.Centre;
+
+                    GL.Begin(GL.LINES);
+                    lineMat.SetPass(0);
+                    GL.Color(new Color(lineMat.color.r, lineMat.color.g, lineMat.color.b, lineMat.color.a));
+                    GL.Vertex3(v1.x, v1.y, v1.z);
+                    GL.Vertex3(v2.x, v2.y, v2.z);
+                    GL.End();
+                }
+            }
+        }
+    }
+
+    // To show the lines in the game window whne it is running
+    void OnPostRender() {
+        if (map.IsReady)
+        {
+            DrawConnectingLines();
+        }
+    }
+}
---
+++ GameManager.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class GameManager : MonoBehaviour {
+
+    //public static GameManager Instance { get; set; }
+
+    public Text text;
+
+    public float lat, lon;
+    public bool foundLoc, gotData;
+
+    private string URL;
+
+    // Use this for initialization
+    void Start () {
+        foundLoc = false;
+        MapReader map = FindObjectOfType<MapReader>();
+        map.ReadInput("data1");
+        //StartCoroutine(StartLocationService());
+    }
+
+    private IEnumerator StartLocationService()
+    {
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("User did not enable location services.");
+            yield break;
+        }
+
+        Input.location.Start();
+        int waitTimeOut = 20;
+
+        while (Input.location.status == LocationServiceStatus.Initializing && waitTimeOut > 0)
+        {
+            yield return new WaitForSeconds(1);
+            waitTimeOut--;
+        }
+
+        if (waitTimeOut <= 0)
+        {
+            Debug.Log("Timed out.");
+            yield break;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.Log("Unable to determine device location.");
+            yield break;
+        }
+
+        lat = Input.location.lastData.latitude;
+        lon = Input.location.lastData.longitude;
+        foundLoc = true;
+        Input.location.Stop();
+
+        yield break;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (foundLoc)
+        {
+            Debug.Log("found location of: " + this.lat.ToString() + " " + this.lon.ToString() + " stopping location coroutine");
+            foundLoc = false;
+            GetMapData();
+        }
+    }
+
+    void GetMapData()
+    {
+        float latHeight = 0.010f;
+        float lonWidth = 0.025f;
+        float lonMin = lon - lonWidth / 2, lonMax = lon + lonWidth / 2, latMin = lat - latHeight / 2, latMax = lat + latHeight / 2;
+        // longitude is "x", latitude is "y"
+        // url bounds in form: longitude min, latitude min, longitude max, latitude max
+        URL = "https://overpass-api.de/api/map?bbox=" + lonMin.ToString() + "," + latMin.ToString() + "," 
+            + lonMax.ToString() + "," + latMax.ToString();
+        Debug.Log(URL);
+
+        StartCoroutine(MapAPICall());
+    }
+
+    IEnumerator MapAPICall()
+    {
+        UnityWebRequest uwr = UnityWebRequest.Get(URL);
+        String filePath = Application.persistentDataPath + "/data1.txt"; // this needs to change
+        uwr.downloadHandler = new DownloadHandlerFile(filePath);
+        yield return uwr.SendWebRequest();
+
+        if (uwr.isHttpError || uwr.isNetworkError)
+        {
+            Debug.Log(uwr.error);
+            yield break;
+        }
+        else
+        {
+            Debug.Log("Saved data to: " + filePath);
+            MapReader map = FindObjectOfType<MapReader>();
+            map.ReadInput(filePath);
+            yield break;
+        }
+    }
+}
