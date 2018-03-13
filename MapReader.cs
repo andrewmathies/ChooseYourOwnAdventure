147fb56dcc3f6c11578da643198d6bb3d2e5ccc3
Andrew Mathies <awmathie@indiana.edu>
2018-03-13 12:18:26 -0400

    first commit

---
+++ MapReader.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+using System.IO;
+
+/*
+    Copyright (c) 2017 Sloan Kelly
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+class MapReader : MonoBehaviour
+{
+    [HideInInspector]
+    public Dictionary<ulong, OsmNode> nodes;
+
+    [HideInInspector]
+    public List<OsmWay> ways;
+    
+    [HideInInspector]
+    public OsmBounds bounds;
+
+    public bool IsReady { get; private set; }
+
+    private DrawLines drawer;
+
+	//// Use this for initialization
+	//void Start ()
+ //   {
+        
+ //   }
+
+    public void ReadInput(string filePath)
+    {
+        IsReady = false;
+        nodes = new Dictionary<ulong, OsmNode>();
+        ways = new List<OsmWay>();
+
+        XmlDocument doc = new XmlDocument();
+        var txtAsset = Resources.Load<TextAsset>(filePath);
+       // Debug.Log(File.ReadAllText(filePath));
+        doc.LoadXml(txtAsset.text);
+
+        SetBounds(doc.SelectSingleNode("/osm/bounds"));
+        GetNodes(doc.SelectNodes("/osm/node"));
+        GetWays(doc.SelectNodes("/osm/way"));
+
+        Debug.Log("successfully parsed data! now drawing");
+
+        IsReady = true;
+    }
+
+    void GetWays(XmlNodeList xmlNodeList)
+    {
+        foreach (XmlNode node in xmlNodeList)
+        {
+            OsmWay way = new OsmWay(node);
+            ways.Add(way);
+        }
+    }
+
+    void GetNodes(XmlNodeList xmlNodeList)
+    {
+        foreach (XmlNode n in xmlNodeList)
+        {
+            OsmNode node = new OsmNode(n);
+            nodes[node.ID] = node;
+        }
+    }
+
+    void SetBounds(XmlNode xmlNode)
+    {
+        bounds = new OsmBounds(xmlNode);
+    }
+}
